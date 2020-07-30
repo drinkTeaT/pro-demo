27d1ee4fbe034fadfc70f663dee787721234738e
EDZ <111>
2020-07-30 18:02:47 +0800

    歇后语 词语url

--- src/main/java/com/tacbin/pro/controller/testdata/TestDataController.java
+++ src/main/java/com/tacbin/pro/controller/testdata/TestDataController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author EDZ
  * @description
@@ -15,28 +18,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/testData")
 public class TestDataController {
     @RequestMapping(method = RequestMethod.GET, path = "/genPersonInfo")
-    public PersonEntity genPersonInfo() {
-        PersonEntity personEntity = new PersonEntity();
-        // 名字
-        GenericGenerator generator = ChineseNameGenerator.getInstance();
-        personEntity.setName(generator.generate());
-        // 地址
-        generator = ChineseAddressGenerator.getInstance();
-        personEntity.setHomeAddr(generator.generate());
-        // id card
-        generator = ChineseIDCardNumberGenerator.getInstance();
-        personEntity.setIdCardNumber(generator.generate());
-        // phone number
-        generator = ChineseMobileNumberGenerator.getInstance();
-        personEntity.setPhoneNumber(generator.generate());
-        // email
-        generator = EmailAddressGenerator.getInstance();
-        personEntity.setEmailAddr(generator.generate());
-        // englishName
-        generator = EnglishNameGenerator.getInstance();
-        personEntity.setEnglishName(generator.generate());
-        // face
-        // https://drive.google.com/drive/folders/1wSy4TVjSvtXeRQ6Zr8W98YbSuZXrZrgY
-        return personEntity;
+    public List<PersonEntity> genPersonInfo() {
+        List<PersonEntity> personEntityList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            PersonEntity personEntity = new PersonEntity();
+            // 名字
+            GenericGenerator generator = ChineseNameGenerator.getInstance();
+            personEntity.setName(generator.generate());
+            // 地址
+            generator = ChineseAddressGenerator.getInstance();
+            personEntity.setHomeAddr(generator.generate());
+            // id card
+            generator = ChineseIDCardNumberGenerator.getInstance();
+            personEntity.setIdCardNumber(generator.generate());
+            // phone number
+            generator = ChineseMobileNumberGenerator.getInstance();
+            personEntity.setPhoneNumber(generator.generate());
+            // email
+            generator = EmailAddressGenerator.getInstance();
+            personEntity.setEmailAddr(generator.generate());
+            // englishName
+            generator = EnglishNameGenerator.getInstance();
+            personEntity.setEnglishName(generator.generate());
+            // face
+            // https://drive.google.com/drive/folders/1wSy4TVjSvtXeRQ6Zr8W98YbSuZXrZrgY}
+            personEntityList.add(personEntity);
+        }
+        // https://github.com/pwxcoo/chinese-xinhua/blob/master/data/ci.json 歇后语 词语
+        return personEntityList;
     }
 }
