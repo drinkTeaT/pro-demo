ff35c2f46287cd0acfb55acb379f99c65c297b5a
EDZ <111>
2020-07-31 17:07:20 +0800

    添加git ignore

--- src/main/java/com/tacbin/pro/controller/websocket/GreetingController.java
+++ src/main/java/com/tacbin/pro/controller/websocket/GreetingController.java
@@ -4,7 +4,6 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
 
